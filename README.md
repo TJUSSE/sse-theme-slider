661da61ebf42894fcd69f20b2ec430dc847bf668
Josh Ouyang <JoshOY789@gmail.com>
2015-08-22 22:48:00 +0800

    Update dependencies.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sse-theme-slider
-SSE web theme slider repo.
+
+SSE web theme slider repository.
+
+## Usage
+
+```bash
+$ bower install
+```
+
+## Dependencies
+
+- font-awesome 4.4.0
+- jquery 1.9.1
--- src/js/sse-slider.js
+++ src/js/sse-slider.js
@@ -12,7 +12,9 @@
 
   $slider = window.g.$sliderScope;
 
-  $(document).ready(function() {});
+  $(document).ready(function() {
+    return $('.sse-slider').append('<div class="sse-slider--indicator"> <div class="spinner"> <div class="spinner-container container1"> <div class="circle1"></div> <div class="circle2"></div> <div class="circle3"></div> <div class="circle4"></div> </div> <div class="spinner-container container2"> <div class="circle1"></div> <div class="circle2"></div> <div class="circle3"></div> <div class="circle4"></div> </div> <div class="spinner-container container3"> <div class="circle1"></div> <div class="circle2"></div> <div class="circle3"></div> <div class="circle4"></div> </div> </div> </div>');
+  });
 
 }).call(this);
 
